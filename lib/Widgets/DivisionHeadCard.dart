7ee40e065eade1f039b2ac9f5cdad1dc6c1d9781
ShrutiKhanduja <shrutikhanduja3859@gmail.com>
2020-08-16 15:14:57 +0530

    some changes

--- lib/Widgets/DivisionHeadCard.dart
+++ lib/Widgets/DivisionHeadCard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:ledgerapp/Classes/Division.dart';
 import 'package:ledgerapp/Screens/DealersList.dart';
 
@@ -35,41 +36,40 @@ class _DivisionHeadCardState extends State<DivisionHeadCard> {
     final pHeight = MediaQuery.of(context).size.height;
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        elevation: 15,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              InkWell(
-                child: Text(
-                  widget.item.name,
-                  style: TextStyle(fontSize: pHeight * 0.03),
+      child: InkWell(
+
+
+            onTap: () {
+              companies.clear();
+              companies.add(widget.item.name);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      dealerlistScreen(
+
+                          companies
+                      ),
                 ),
-                onTap: () {
-                  companies.clear();
-                  companies.add(widget.item.name);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          dealerlistScreen(
+              );
 
-                              companies
-                          ),
-                    ),
-                  );
+            },
 
-                }
-              ),
-              SizedBox(
-                height: pHeight * 0.02,
-              ),
-            ],
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          elevation: 15,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height:pHeight*0.01),
+                Container(alignment:Alignment.center,child: Text(widget.item.name,textAlign:TextAlign.center,style: GoogleFonts.lato(textStyle: TextStyle(color:Colors.black,fontSize: 0.023*pHeight),),)),
+                SizedBox(height:pHeight*0.01)
+              ],
+            ),
           ),
         ),
       ),
